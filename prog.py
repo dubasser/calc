4a0f040a603b6f5bfb4c0fbd2b0feb9ea54ce3d1
dubasser <90547503+dubasser@users.noreply.github.com>
2021-09-12 12:46:06 +0500

    Add files via upload
    -Добавлены текстовые подсказки для пользователя;
    -Добавлена функция сложения и умножения чисел;

--- prog.py
+++ prog.py
@@ -7,7 +7,15 @@ def subtract(x,y):
 def abs_average(x,y):
     return (abs(float(x))+abs(float(y)))/2
 
+def multiplicate(x,y):
+    return float(x)*float(y)
+
+def divide(x,y):
+    return float(x)/float(y)
+print("Vvedite x:")
 x=input()
+print("Vvedite y:")
 y=input()
 print("Summa:"+str(add(x,y))+"   Raznost:"+str(subtract(x,y))+"   Srednee modylei:"+str(abs_average(x,y)))
+print("Proizvedenie:"+str(multiplicate(x,y))+"   Chastnoe:"+str(divide(x,y)))
 input()
